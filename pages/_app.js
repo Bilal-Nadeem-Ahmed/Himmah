bff6279b96d888d93024e2bf7847945203de617c
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-16 13:27:39 +0000

    added bootstrap

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import Head from "next/head";
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+function MyApp({ Component, pageProps }) {
+  return( 
+  <>
+  <Head>
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+</Head>
+  <Component {...pageProps} />
+  </>)
+}
+
+export default MyApp
