ed91a1089260edf6e05e3c3f3575241c87effc89
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-18 15:49:34 +0000

    added carousel
    must personalise the content now

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css'
 import Navigation from '../Components/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../Components/Footer';
+import {Carousel} from 'react-bootstrap'
 
 export default function Home() {
   return (
@@ -20,10 +21,48 @@ export default function Home() {
 
       <main className={styles.main}>
       <Navigation/>
+      
       <div className ={'big'}></div>
       <div id='tacklingpoverty'>tackling poverty</div>
       <div id="salaamshalomkitchen"></div>
       <div id='tacklinghate'>tackling hate</div>
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://source.unsplash.com/random/800x600"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://source.unsplash.com/random/800x600"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://source.unsplash.com/random/800x600"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
       </main>
 
       <footer className={styles.footer}>
